c867fdfde898f03a074bd7d9f0dd6c3ccfbddab2
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-12-15 10:25:25 +0100

    add Serilog

--- src/ProjectManagement.API/Startup.cs
+++ src/ProjectManagement.API/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Serilog;
 
 namespace ProjectManagement.API
 {
@@ -46,6 +40,8 @@ namespace ProjectManagement.API
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProjectManagement.API v1"));
             // end swagger
+
+            app.UseSerilogRequestLogging();
 
             app.UseHttpsRedirection();
 
