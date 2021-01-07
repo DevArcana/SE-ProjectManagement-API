d9369329e78f9669820d03b66741cd016dd57ac9
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2021-01-07 11:28:02 +0100

    add migration

---
+++ src/ProjectManagement.API/Infrastructure/Persistence/Migrations/20210107102744_IssuesMoveRelation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectManagement.API.Infrastructure.Persistence.Migrations
+{
+    public partial class IssuesMoveRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
