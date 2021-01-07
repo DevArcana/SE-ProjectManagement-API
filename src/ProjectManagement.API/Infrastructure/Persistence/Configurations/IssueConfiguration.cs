978eb582da6018894717d9959ef2ea8eb16d61f6
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2021-01-07 11:27:29 +0100

    move relation configuration to project from issue

--- src/ProjectManagement.API/Infrastructure/Persistence/Configurations/IssueConfiguration.cs
+++ src/ProjectManagement.API/Infrastructure/Persistence/Configurations/IssueConfiguration.cs
@@ -9,7 +9,6 @@ namespace ProjectManagement.API.Infrastructure.Persistence.Configurations
         public void Configure(EntityTypeBuilder<Issue> builder)
         {
             builder.HasKey(x => x.Id);
-            builder.HasOne(x => x.Project).WithMany().IsRequired();
             builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
             builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
             builder.Property(x => x.Description).IsRequired(false).HasMaxLength(5000);
