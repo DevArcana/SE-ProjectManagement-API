c55e5e461d3caca956f787b71e7d4953ac737ce6
CptAs <arpek2121@gmail.com>
2021-01-15 19:35:34 +0100

    Added Configuration file

---
+++ src/ProjectManagement.API/Infrastructure/Persistence/Configurations/UserProjectAccessConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjectManagement.API.Domain.Projects.Entities;
+
+namespace ProjectManagement.API.Infrastructure.Persistence.Configurations
+{
+    public class UserProjectAccessConfiguration : IEntityTypeConfiguration<UserProjectAccess>
+    {
+        public void Configure(EntityTypeBuilder<UserProjectAccess> builder)
+        {
+            builder.HasKey(x => new {x.User, x.Project});
+            builder.HasOne(x => x.User).WithMany().IsRequired();
+            builder.HasOne(x => x.Project).WithMany().IsRequired();
+        }
+    }
+}
