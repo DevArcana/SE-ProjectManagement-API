42dedee0f7971ab96c9c0e74b6829c10211767d1
CptAs <arpek2121@gmail.com>
2021-01-15 18:55:06 +0100

    Added UserProjectAccess.cs entity

---
+++ src/ProjectManagement.API/Domain/Projects/Entities/UserProjectAccess.cs
@@ -0,0 +1,22 @@
+﻿using ProjectManagement.API.Domain.Users.Entities;
+
+namespace ProjectManagement.API.Domain.Projects.Entities
+{
+    public class UserProjectAccess
+    {
+        public ApplicationUser User { get; private set; }
+        public Project Project { get; private set; }
+
+        private UserProjectAccess()
+        {
+            
+        }
+
+        private UserProjectAccess(ApplicationUser u, Project p)
+        {
+            User = u;
+            Project = p;
+        }
+        
+    }
+}
